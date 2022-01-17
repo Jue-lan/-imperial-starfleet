e1ef18a8ed6e55ce037a8292bb4ccec3533e9e0b
jue-lan <juelan.brown@nyu.edu>
2022-01-17 11:53:06 -0500

    completed part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeathSquareComponent } from './death-square/death-square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeathSquareComponent
   ],
   imports: [
     BrowserModule
